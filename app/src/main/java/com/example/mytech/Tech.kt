09c4b1afc15e4947a01c60ca841e7e43f946cb59
albarfikri4 <fikrialbar4@gmail.com>
2021-03-07 10:27:16 +0700

    First commit

---
+++ app/src/main/java/com/example/mytech/Tech.kt
@@ -0,0 +1,9 @@
+package com.example.mytech
+
+class Tech (
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+        )
+
+
---
+++ app/src/main/java/com/example/mytech/TechData.kt
@@ -0,0 +1,16 @@
+package com.example.mytech
+
+object TechsData{
+    private val TechNames = arrayOf(
+        "ASUS ProArt StudioBook Pro 15 Mobile Workstation W500G5T-HC005R"
+    )
+
+
+    private val TechDetails = arrayOf(
+        "Seri ASUS StudioBook adalah pembangkit tenaga listrik perkasa yang menghidupkan ide. Dilengkapi dengan NVIDIA® Quadro GPU, dan menawarkan layar NanoEdge dengan bingkai tipis untuk visual yang imersif. Kombinasi grafis mutakhir dan portabilitas membuatnya sempurna untuk para profesional kreati"
+    )
+
+    private val TechImages = intArrayOf(
+        R.drawable.asus_proart15
+    )
+}
